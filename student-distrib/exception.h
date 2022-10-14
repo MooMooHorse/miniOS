582faed149a8a487a016fb430c946f5b68100382
Ren <haor2@illinois.edu>
2022-10-13 20:54:10 -0500

    switch devices

--- student-distrib/exception.h
+++ student-distrib/exception.h
@@ -1,4 +1,11 @@
 #ifndef _X86_EXCEPTION_H
 #define _X86_EXCEPTION_H
 
+#include "types.h"
+
+struct old_regs{
+    uint32_t oeax,oecx,oedx,oebp;
+    uint32_t 
+};
+
 #endif
---
+++ student-distrib/phandler.h
@@ -0,0 +1,9 @@
+#ifndef _PHANLDLER_H
+#define _PHANLDLER_H
+
+#include "types.h"
+
+fastcall unsigned int
+do_exception();
+
+#endif

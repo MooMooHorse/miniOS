c93ffe228172bb05cd12f2261df89aa2f6e8481a
brant-skywalker <1773159722@qq.com>
2022-10-17 01:45:02 -0500

    Added comments for RTC.

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -21,19 +21,13 @@ void rtc_init(void) {
     // disable all interrupts
     cli();
 
-    // // SET RTC TO 2 HZ AND ENABLE OSCILLATOR
-    // outb(RTC_REG_A, RTC_PORT);
-    // char prev = inb(RTC_PORT + 1);
-    // outb(RTC_REG_A, RTC_PORT);
-    // outb((prev & 0x80) | 0x2F, RTC_PORT + 1); // set the rate to 2Hz, enable oscillator
-
     // ENABLE PERIODIC INTERRUPTS
     // Default rate of 1024 Hz
     outb(RTC_REG_B, RTC_PORT);
     char prev = inb(RTC_PORT + 1);
 
     outb(RTC_REG_B, RTC_PORT);
-    outb(prev | 0x40, RTC_PORT + 1);
+    outb(prev | 0x40, RTC_PORT + 1);  // Turn on bit 6 of register B.
 
     // enable IRQ8
     enable_irq(RTC_IRQ);
@@ -49,8 +43,13 @@ void rtc_init(void) {
  */
 void rtc_handler(void) {
     uint32_t flags;
+
     cli_and_save(flags);
-    test_interrupts();
-    send_eoi(RTC_IRQ);
-    restore_flags(flags);
+
+    send_eoi(RTC_IRQ);  // Disable interrupts and save flags.
+
+    // Handle RTC interrupt.
+    test_interrupts();  // Only for testing purposes.
+
+    restore_flags(flags);  // Restore flags. (Also the IF bit.)
 }

7976ecd1d855559e8f0c0ede2c1a71220575a80a
brant-skywalker <1773159722@qq.com>
2022-10-23 17:02:29 -0500

    Integrated device driver interface and updated tests.

--- student-distrib/terminal.h
+++ student-distrib/terminal.h
@@ -13,14 +13,20 @@
 #define _TERMINAL_H
 
 #include "keyboard.h"
+#include "filesystem.h"
 
+typedef struct terminal {
+    fops_t ioctl;
+} terminal_t;
+
+extern terminal_t terminal;
 
 // Device driver interfaces.
 void terminal_init(void);
-int32_t terminal_open(const uint8_t* filename);
-int32_t terminal_close(int32_t fd);
-int32_t terminal_read(int32_t fd, void* buf, int32_t nbytes);
-int32_t terminal_write(int32_t fd, const void* buf, int32_t nbytes);
+int32_t terminal_open(fd_t* fd, const uint8_t* buf, int32_t nbytes);
+int32_t terminal_close(fd_t* fd);
+int32_t terminal_read(fd_t* fd, void* buf, int32_t nbytes);
+int32_t terminal_write(fd_t* fd, const void* buf, int32_t nbytes);
 
 
 #endif  /* _TERMINAL_H */

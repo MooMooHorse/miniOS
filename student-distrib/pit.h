cd602fd3999a1539536b310557a959015b50afc4
brant-skywalker <1773159722@qq.com>
2022-11-21 19:52:28 -0600

    Removed redundant macro.

--- student-distrib/pit.h
+++ student-distrib/pit.h
@@ -1,11 +1,10 @@
 #ifndef _PIT_H
 #define _PIT_H
 
-#define PIT_IRQ     0x00
+#define PIT_IRQ     0x0
 #define PIT_DATA    0x40        // Channel 0 data port (rw).
 #define PIT_CMD     0x43        // Mode/command register (w).
 #define PIT_FREQ    1193182
-#define PIT_BIN     0x0
 #define PIT_BIN     0x0         // Binary mode.
 #define PIT_SQR     (0x3 << 1)  // Operating mode: Square wave generator.
 #define PIT_LH      (0x3 << 4)  // Access mode: lobyte/hibyte.

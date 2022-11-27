1c404cc1912e55287d1326aebd9ea6510e535f76
brant-skywalker <1773159722@qq.com>
2022-11-26 21:02:37 -0600

    Added documentation.

--- student-distrib/scheduler.c
+++ student-distrib/scheduler.c
@@ -1,5 +1,15 @@
+/*!
+ * @brief This file contains implementations for scheduler.
+ */
 #include "scheduler.h"
 #include "terminal.h"
+
+/*!
+ * @brief This function finds a new process and switch execution to it.
+ * @param None.
+ * @return None.
+ * @sideeffect Swaps kernel stacks. Saves & Restores callee-saved registers.
+ */
 void
 scheduler(void) {
     pcb_t* p;
@@ -9,15 +19,14 @@ scheduler(void) {
 
     if (0 == pid) { return; }  // Wait until the shell is spawned.
 
-    
     // Update status of process in the current terminal.
     cur = PCB(pid);
     terminal[terminal_index].pid = pid;
     cur->state = RUNNABLE;
-    
-    if(-1==terminal_update(cur->terminal)){
-        printf("scheduler : terminal fatal error\n");
-        while(1);
+
+    if (-1 == terminal_update(cur->terminal)) {
+        printf("scheduler: terminal fatal error\n");
+        while (1);
     }
 
     for (i = 1; i <= PCB_MAX; ++i) {
@@ -26,15 +35,13 @@ scheduler(void) {
             break;
         }
     }
-    
+
     pid = (pid - 1 + i) % PCB_MAX + 1;  // Commit update of `pid`.
 
-    /* printf("context switch: #%u --> #%u\n", get_pid(), pid); */
-    if(-1==prog_video_update((PCB(pid))->terminal)){
+    if (-1 == prog_video_update((PCB(pid))->terminal)) {
         printf("scheduler : terminal fatal error\n");
-        while(1);
+        while (1);
     }
-
 
     p->state = RUNNING;
 

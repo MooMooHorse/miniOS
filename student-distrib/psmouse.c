96bef8cc7b9d4bcfa6d49b5c56789030be3041b6
brant-skywalker <1773159722@qq.com>
2022-12-05 00:43:48 -0600

    Done template PS/2 mouse driver.

--- student-distrib/psmouse.c
+++ student-distrib/psmouse.c
@@ -1,6 +1,12 @@
+/*!
+ * @brief This file contains implementations for PS/2 mouse.
+ */
 #include "psmouse.h"
 
-#define PSMOUSE_TIMEOUT  100000
+#define PSMOUSE_TIMEOUT     100000
+#define SIGN_EXTENSION(x)   ((x) | 0xFFFFFF00)
+#define SCREEN_WIDTH        80
+#define SCREEN_HEIGHT       25
 
 #define PSMOUSE_WAIT_IN                                      \
     do {                                                     \
@@ -28,6 +34,18 @@
         inb(PSMOUSE_DATA);                  \
     })
 
+#define PSMOUSE_PACKET_INVALID(x)  \
+    (((x) & (PSMOUSE_XOF | PSMOUSE_YOF)) || !((x) & 0x8))
+
+static int32_t screen_x;
+static int32_t screen_y;
+
+/*!
+ * @brief This function initializes the PS/2 mouse.
+ * @param None.
+ * @return None.
+ * @sideeffect Consumes data in ports 0x60 and 0x64.
+ */
 void
 psmouse_init(void) {
     uint8_t stat;
@@ -49,7 +67,42 @@ psmouse_init(void) {
     enable_irq(PSMOUSE_IRQ);
 }
 
+/*!
+ * @brief PS/2 mouse interrupt handler.
+ * @param None.
+ * @return None.
+ * @sideeffect Modifies video memory.
+ */
 void
 psmouse_handler(void) {
-    printf("FUCK\n");
+    uint8_t stat;
+    int32_t delta_x;
+    int32_t delta_y;
+    send_eoi(PSMOUSE_IRQ);
+
+    // Get the first three packet bytes.
+    stat = PSMOUSE_READ;
+    delta_x = PSMOUSE_READ;
+    delta_y = PSMOUSE_READ;
+
+    if (PSMOUSE_PACKET_INVALID(stat)) {
+        return;  // Discard the packet since it is invalid.
+    }
+
+    delta_x = (stat & PSMOUSE_XS) ? SIGN_EXTENSION(delta_x) : delta_x;
+    delta_y = (stat & PSMOUSE_YS) ? SIGN_EXTENSION(delta_y) : delta_y;
+
+    unlight_pixel(screen_x, screen_y);
+    screen_x += delta_x;
+    screen_y -= delta_y;
+
+    // Reset to edges if out of bounds.
+    screen_x = (1 > screen_x) ? 1 : (SCREEN_WIDTH < screen_x) ? SCREEN_WIDTH : screen_x;
+    screen_y = (0 > screen_y) ? 0 : (SCREEN_HEIGHT - 1 < screen_y) ? SCREEN_HEIGHT - 1 : screen_y;
+    light_pixel(screen_x, screen_y);
+
+    // Handle left clicks...
+    if (stat & PSMOUSE_LB) {
+        // ...
+    }
 }
--- student-distrib/psmouse.h
+++ student-distrib/psmouse.h
@@ -1,3 +1,6 @@
+/*!
+ * @brief This file contains declarations for PS/2 mouse.
+ */
 #ifndef _PSMOUSE_H
 #define _PSMOUSE_H
 
@@ -13,11 +16,12 @@
 #define PSMOUSE_RB       (1 << 1)       // Right button.
 #define PSMOUSE_MB       (1 << 2)       // Middle button.
 #define PSMOUSE_XS       (1 << 4)       // X sign bit.
-#define PSMOUSE_kS       (1 << 5)       // Y sign bit.
+#define PSMOUSE_YS       (1 << 5)       // Y sign bit.
 #define PSMOUSE_XOF      (1 << 6)       // X overflow.
 #define PSMOUSE_YOF      (1 << 7)       // Y overflow.
 #define PSMOUSE_WF       (1 << 1)       // Write flag.
 #define PSMOUSE_RF       (1 << 0)       // Read flag.
+#define PSMOUSE_STAT_EN  (1 << 5)       // Data reporting is enabled.
 #define PSMOUSE_EAD      0xA8           // Enable Auxiliary command.
 #define PSMOUSE_GCS      0x20           // Get Compaq Status byte.
 #define PSMOUSE_DEF      0xF6           // Set Defaults.
